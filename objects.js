9255b474673d0965c9760606ffb06a750b402f40
learn <learn@learn02-7.local>
2017-10-11 16:18:18 -0700

    accel and decel loop

--- objects.js
+++ objects.js
@@ -28,3 +28,41 @@ function carInfo(){
 }
 
 //
+
+
+// while loops and objects
+function carInfo(){
+  var car = {
+    make: "Hyundai",
+    model: "Elantra",
+    year: "2005",
+    color: "Red",
+    speed: 0,
+    accelerateLoop: function(){
+      while (car.speed < 70) {
+        car.speed += 1;
+      }
+      return car.speed;
+    },
+    decelerateLoop: function(){
+      while (car.speed > 0) {
+        car.speed -= 1;
+      }
+      return car.speed;
+    },
+    all: function() {console.log("Your car is a " + this.color + " " + this.year + " " + this.make + " " + this.model + " "); },
+    getSpeed: function() {
+     return {
+       accelerate: function() {
+         car.speed += 10;
+         return car.speed;
+       },
+       brake: function() {
+         car.speed -= 7;
+         return car.speed;
+        }
+      }
+    }
+  }
+  return car;
+}
